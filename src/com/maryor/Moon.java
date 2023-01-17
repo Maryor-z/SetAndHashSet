3c290c1f73d01c60e28cd11a85217cb44e436a80
Maryor-z <ogundipemayowa90@icloud.com>
2023-01-17 15:22:35 +0100

    initial commit

---
+++ src/com/maryor/Moon.java
@@ -0,0 +1,11 @@
+package com.maryor;
+
+public class Moon extends HeavenlyBody {
+
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+
+
+
+}
---
+++ src/com/maryor/SetChallegne.java
@@ -0,0 +1,7 @@
+package com.maryor;
+
+public class SetChallegne {
+    public static void main(String[] args) {
+
+    }
+}
